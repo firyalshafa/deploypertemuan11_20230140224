628d22ef83597fb974db32f77492a3ed5ddff973
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2026-05-18 23:56:30 +0700

    create User entity with @OneToOne relationship to Profile

--- src/main/java/com/deploy/pertemuan11/model/User.java
+++ src/main/java/com/deploy/pertemuan11/model/User.java
@@ -1,4 +1,25 @@
 package com.deploy.pertemuan11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users") // Disarankan 'users' karena 'user' adalah reserved keyword di beberapa DB
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+
+    // AMAN: Menggunakan tipe data Profile, bukan String
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Profile profile;
 }
